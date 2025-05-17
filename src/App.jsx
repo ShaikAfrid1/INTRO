fc6e3af05ab4ab98b4e05760117dd9d96667b47c
afridshaik <shaikafrid500@gmail.com>
2025-05-17 12:51:36 +0530

    Two way binding form elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
 import { useState } from "react";
-import Create from "./components/Create";
-import Read from "./components/Read";
+
 const App = () => {
-  const [users, setusers] = useState([
-    { name: "Afrid", age: 31 },
-    { name: "Aushu", age: 36 },
-    { name: "shafu", age: 24 },
-  ]);
+  const [title, settitle] = useState("");
+  const [Completed, setCompleted] = useState(true);
+  const [gender, setgender] = useState("male");
+  const [city, setcity] = useState("chittoor");
 
   return (
     <div>
-      <Create />
-      <Read users={users} setusers={setusers} />
+      <h1>Create Taks!</h1>
+      <form action="#">
+        <input
+          value={title}
+          onChange={(e) => settitle(e.target.value)}
+          type="text"
+          placeholder="Title"
+        />
+        <br /> <br />
+        <input
+          type="checkbox"
+          checked={Completed}
+          onChange={(e) => setCompleted(e.target.checked)}
+        />
+        Completed
+        <br /> <br />
+        <input
+          type="radio"
+          onChange={(e) => setgender(e.target.value)}
+          checked={gender == "male" ? true : false}
+          value={"male"}
+        />
+        Male
+        <br /> <br />
+        <input
+          type="radio"
+          onChange={(e) => setgender(e.target.value)}
+          checked={gender == "female" ? true : false}
+          value={"female"}
+        />
+        Female
+        <br />
+        <br />
+        <select value={city} onChange={(e) => setcity(e.target.value)}>
+          <option value="chittoor">Chittoor</option>
+          <option value="chennai">Chennai</option>
+          <option value="hyderabad">Hyderabad</option>
+          <option value="banglore">Banglore</option>
+        </select>
+        <br />
+        <br />
+        <button>Create Todo</button>
+      </form>
     </div>
   );
 };
