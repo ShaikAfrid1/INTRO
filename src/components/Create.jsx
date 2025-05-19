64443d2b30f3ee2964908aa6240dd2ab2e6be01b
afridshaik <shaikafrid500@gmail.com>
2025-05-19 20:58:13 +0530

    added function of not able to add blank empty tasks

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
 import { nanoid } from "nanoid";
+import { useForm } from "react-hook-form";
 
 const Create = (props) => {
   const todos = props.todos;
   const settodos = props.settodos;
-  const [title, settitle] = useState("");
-  let submitHandler = (e) => {
-    e.preventDefault();
-    let newtodo = {
-      id: nanoid(),
-      title: title,
-      isCompleted: false,
-    };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  let submitHandler = (data) => {
+    data.isCompleted = false;
+    data.id = nanoid();
 
     let copytodos = [...todos];
-    copytodos.push(newtodo);
+    copytodos.push(data);
     settodos(copytodos);
-    settitle("");
+    reset();
   };
 
   return (
@@ -24,14 +27,16 @@ const Create = (props) => {
       <h1 className="mb-10 text-6xl font-thin ">
         Set <span className="text-red-400">Remainders</span> for <br /> Tasks!
       </h1>
-      <form onSubmit={submitHandler}>
+      <form onSubmit={handleSubmit(submitHandler)}>
         <input
+          {...register("title", { required: "title can not be empty" })}
           className=" p-2 border-b w-full text-2xl font-thin outline-0"
-          value={title}
-          onChange={(e) => settitle(e.target.value)}
           type="text"
           placeholder="Title"
         />
+        <small className="font-thin text-red-400">
+          {errors?.title?.message}
+        </small>
         <br /> <br />
         <button className="mt-10 text-xl px-10 py-2 border rounded hover:bg-gray-700 cursor-pointer active:-scale-x-90 ">
           Create Todo
